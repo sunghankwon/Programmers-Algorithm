a56e3f362f0ba80f70c1fcd7deab969d3db237ca
sunghan <138760807+sunghankwon@users.noreply.github.com>
2024-06-17 17:46:01 +0900

    [level 2] Title: 숫자의 표현, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,11 @@
+function solution(n) {
+  let count = 0;
+  
+  for (let i = 1; i * (i + 1) / 2 <= n; i++) {
+    if ((n - (i * (i + 1)) / 2) % i === 0) {
+      count++;
+    }
+  }
+  
+  return count;
+}
